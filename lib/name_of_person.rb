f55308ef93356a0ef39a0245619b34c05920ddaf
Patrick Bougie <pbougie@users.noreply.github.com>
2019-03-25 19:50:06 -0400

    Require NameOfPerson::HasPersonName

--- lib/name_of_person.rb
+++ lib/name_of_person.rb
@@ -1,5 +1,6 @@
 module NameOfPerson
 end
 
+require 'name_of_person/has_person_name'
 require 'name_of_person/loaders/active_model_has_person_name'
 require 'name_of_person/loaders/active_record_has_person_name'
