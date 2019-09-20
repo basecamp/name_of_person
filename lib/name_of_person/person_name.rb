3df5a58ea41fbf59b8f19caa5fc60f5bd893ba5e
Nick Weiland <nickweiland@gmail.com>
2019-09-20 16:14:07 -0700

    Allows #initials if name is a single letter
    Currently, if a person's name is a single letter, their initials will be blank.  If we want to assume that a single letter is an invalid name for the purposes of this lib, that works, but I noticed this and thought it might make sense to patch.
    
    [(no branch, rebasing master)]

--- lib/name_of_person/person_name.rb
+++ lib/name_of_person/person_name.rb
@@ -42,7 +42,7 @@ module NameOfPerson
 
     # Returns just the initials.
     def initials
-      @initials ||= remove(/(\(|\[).*(\)|\])/).scan(/([[:word:]])[[:word:]]+/i).join
+      @initials ||= remove(/(\(|\[).*(\)|\])/).scan(/([[:word:]])[[:word:]]*/i).join
     end
 
     # Returns a mentionable version of the familiar name
