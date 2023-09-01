f35362b2979e3d4f7b18f882ae30bff4cfef245c
Rafael Mendonça França <rafael@rubyonrails.org>
2023-09-01 18:44:44 -0400

    Fix logic (#15)
    We should not use the on_load hook if we aren't in Rails 7.1.

--- lib/name_of_person/loaders/active_model_has_person_name.rb
+++ lib/name_of_person/loaders/active_model_has_person_name.rb
@@ -1,9 +1,9 @@
 if ActiveSupport.gem_version < "7.1.0.alpha"
-  ActiveSupport.on_load(:active_model) do
-    include NameOfPerson::HasPersonName
-  end
-else
   if defined?(ActiveModel)
     ActiveModel::Model.send :include, NameOfPerson::HasPersonName
+  end
+else
+  ActiveSupport.on_load(:active_model) do
+    include NameOfPerson::HasPersonName
   end
 end
