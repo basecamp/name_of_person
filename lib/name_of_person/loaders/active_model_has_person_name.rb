d5abdaa607f4fa8dab094475e0c7a1e0d5c5981c
Bryan Haskin <bhaskin@referralexchange.com>
2023-10-05 17:04:05 -0700

    fixed version check for 2.7.4

--- lib/name_of_person/loaders/active_model_has_person_name.rb
+++ lib/name_of_person/loaders/active_model_has_person_name.rb
@@ -1,4 +1,4 @@
-if ActiveSupport.gem_version < "7.1.0.alpha"
+if ActiveSupport.gem_version < Gem::Version.new('7.1.0.alpha')
   if defined?(ActiveModel)
     ActiveModel::Model.send :include, NameOfPerson::HasPersonName
   end
